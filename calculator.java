2ff5a6cbba6305ffbc98da41d513268b3b8e92ff
Anuska Ghosh <98759881+Anuska93@users.noreply.github.com>
2023-10-01 22:16:36 +0530

    Create calculator.java

---
+++ calculator.java
@@ -0,0 +1,43 @@
+// a program to take two inputs and operator from the user and performs the operation
+// then prints the result for the users
+
+
+import java.util.Scanner;
+
+public class calculator {
+    public static void main(String[] args){
+
+        double res=0;
+        Scanner s= new Scanner(System.in);
+        System.out.println("Enter first number:");
+
+        double s1= s.nextDouble();
+
+        System.out.println("Enter second number: ");
+
+        double s2= s.nextDouble();
+
+        System.out.println("Enter operation: \n 1.Addition \n 2.Subtraction \n 3. Multiplication \n 4. Division");
+        String op= s.next();
+
+
+        switch (op){
+            case "+":
+                 res = s1 + s2;
+                break;
+            case "-":
+               res=s1-s2;
+                break;
+            case "*":
+               res=s1*s2;
+                break;
+            case "/":
+                res=s1/s2;
+                break;
+            default:
+                System.out.println("Invalid operator please check once again");
+
+        }
+        System.out.println("Result is: "+res);
+    }
+}
