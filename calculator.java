8ce26ff3bcce3f94c15c605e006a47840f5fed2e
Anuska Ghosh <98759881+Anuska93@users.noreply.github.com>
2023-11-03 19:51:11 +0530

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -17,7 +17,7 @@ public class calculator {
 
         double s2= s.nextDouble();
 
-        System.out.println("Enter operation: \n 1.Addition \n 2.Subtraction \n 3. Multiplication \n 4. Division");
+        System.out.println("Enter operation: (+,-,*,/)");
         String op= s.next();
 
 
